8522c9df35543d2f2a6a09b355db940c7419ef90
Duncan <tdb0021@auburn.edu>
2021-08-17 19:13:38 -0500

    Update interface.py

--- selfdrive/car/ram/interface.py
+++ selfdrive/car/ram/interface.py
@@ -82,7 +82,6 @@ class CarInterface(CarInterfaceBase):
 #      return []  
 
     can_sends = self.CC.update(c.enabled, self.CS, self.frame, c.actuators, c.cruiseControl.cancel, 
-                               c.hudControl.leftLaneVisible, c.hudControl.rightLaneVisible, 
-                               c.hudControl.autoHighBeamBit)
+                               c.hudControl.leftLaneVisible, c.hudControl.rightLaneVisible)
     self.frame += 1
     return can_sends
